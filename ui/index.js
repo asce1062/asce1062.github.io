1758a8e2aa89994230f87db43036f2e9e7365310
asce1062 <alex.mbugua@andela.com>
2024-09-10 20:46:26 +0300

    Improve styling

--- ui/index.js
+++ ui/index.js
@@ -1,43 +1,48 @@
 function gid(e) {
-    return document.getElementById(e);
+  return document.getElementById(e);
 }
 
 function ev(el, e, f) {
-    return el.addEventListener(e, f);
+  return el.addEventListener(e, f);
 }
 
-var player = gid("audio");
-var source = gid("source");
+var player = gid('audio');
+var source = gid('source');
 
 function setSrc(src) {
-    if (source.src != src) {
-        source.src = src;
-        audio.load();
-
-        if (audio.paused) {
-            audio.play();
-        } else {
-            audio.pause();
-        }
-        return;
-    }
+  if (source.src != src) {
+    source.src = src;
+    audio.load();
 
     if (audio.paused) {
-        audio.play();
+      audio.play();
     } else {
-        audio.pause();
+      audio.pause();
     }
+    return;
+  }
+
+  if (audio.paused) {
+    audio.play();
+  } else {
+    audio.pause();
+  }
 }
 
-ev(audio, "timeupdate", function() {
-    var width = $(".playing").width();
-    $(".playing").css({ "box-shadow": "inset " + ((width / audio.duration) * audio.currentTime) + "px 0 #C28754" });
+ev(audio, 'timeupdate', function () {
+  var width = $('.playing').width();
+  $('.playing').css({
+    'box-shadow': 'inset ' + (width / audio.duration) * audio.currentTime + 'px 0 #C28754',
+  });
 });
 
-$(".track").click(function() {
-    $(".playing").css({ "box-shadow": "inset 2px 2px white, inset -2px -2px white, inset 2px -2px white, inset -2px 2px white, inset" });
-    $(".playing").addClass("default");
-    $(".playing").removeClass("playing");
-    $(this).addClass("playing");
-    $(this).addClass("default");
+$('.track').click(function () {
+  $('.playing').css({
+    'box-shadow':
+      'inset 2px 2px white, inset -2px -2px white, inset 2px -2px white, inset -2px 2px white, inset',
+  });
+  $('.playing').addClass('default');
+  $('.playing').removeClass('playing');
+  $(this).addClass('playing');
+  $(this).addClass('default');
 });
